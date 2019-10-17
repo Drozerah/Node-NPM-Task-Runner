2875785cc8238920cd9e3d9a64636fee02b12a1b
Drozerah <drozerah@gmail.com>
2019-10-17 16:44:34 +0200

    refactor: build tasks
    - bin/index.js:
    	- better errors handling
    	- clean code, centralize declaration
    	- get output dir from config
    	- add hash from config
    	- compress CSS from config
    	- use source mapping from config
    	- add programmatically css stylesheet into file.html
    	- use regex when writing files content but not for the bundle.js
    	(todo)
    - bin/lib.js:
    	- add findFilesInDir() function
    	- add source link
    - package.json:
    	- add config options

--- bin/index.js
+++ bin/index.js
@@ -17,110 +17,307 @@ const chalk = require('chalk')
  * Modules
  */
 const dirStrName = require('./lib.js').dirStrName
-
+const addHash = require('./lib.js').addHash
+const findFilesInDir = require('./lib.js').findFilesInDir
+const settings = require("../.vscode/settings.json")
 /**
- * Create the build output directory from package.json
- * config build section directory name
+ * Get config options 
  */
-let outputDir = `../${process.env.npm_package_config_build_outputDir}`
-outputDir = path.join(__dirname, outputDir)
-
+const outputDir = `./${process.env.npm_package_config_build_outputDir}` // string to path
+const isHash = JSON.parse(process.env.npm_package_config_build_hash) // boolean
+const isCompressedCSS = JSON.parse(process.env.npm_package_config_build_compressedCSS) === false ? 0 : 1 // boolean to number
+const settingsFormats = settings['liveSassCompile.settings.formats'] // array
+const isSourceMap = settings['liveSassCompile.settings.generateMap'] // boolean
 /**
-* Build task 
+*  Some references
 */
-function buildTask () {
-  let htmlNewContent = null
+// generate hash from current time
+const hash = new Date().getTime()
+// store content (steps 4, 8, 9)
+let fileNewContent = undefined
+/**
+* Files references 
+*/
+// get/find the HTML file from ./src
+const HTMLFileName = dirStrName(findFilesInDir('./src','.html')[0])
+const HTMLSourceFIlePath = `./${findFilesInDir('./src','.html')[0].replace(/\\/gim, '/')}`
+const HTMLDestinationFilePath = `${outputDir}/${HTMLFileName}`
+// working with CSS file
+// we use the prefix string (file name) of the file.scss
+// found in ./src to create our prefix.css files
+let CSSPrefix = findFilesInDir('./src','.scss').filter(str => {
+  if (!str.includes('_')) return str
+})
+// file prefix 
+CSSPrefix = dirStrName(CSSPrefix[0]).split('.')[0]
+// CSS files
+const CSSSource = `${CSSPrefix}.css`
+const CSSFileName = CSSPrefix + settingsFormats[isCompressedCSS].extensionName
+const CSSSourceFilePath = `.${settingsFormats[isCompressedCSS].savePath}/${CSSFileName}`
+const CSSDestinationFilePath = `${outputDir }/${addHash(CSSFileName, isHash, hash)}`
+/**
+* Build tasks 
+*/
+function buildTasks () {
   return async.series([
-    callback => { // Step 0 remove existing './dist' folder
-      rimraf(outputDir, err => { // remove folder if already exists
-        if (err) callback(true, `ERROR-step0 ${err}`)
+    /**
+    * Step 1
+    * Remove './dist' directory if any
+    */
+    callback => {
+      rimraf(outputDir, err => {
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-1`))
+          callback(true, err)
+        } 
         callback(false, chalk.yellow('Start build task:'))
       })
     },
-    callback => { // Step 1 create build destination folder './dist'
+    /**
+    * Step 2
+    * Make directory './dist'  
+    */
+    callback => {
       fs.mkdir(outputDir, err => {
-        if (err) callback(true, `ERROR-step1 ${err}`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-2`))
+          callback(true, err)
+        }
         callback(false, `${dirStrName(outputDir)} created`)
       })
     },
-    callback => { // Step 2 copy index.html to destination folder
-      const fileName = 'index.html'
-      const source = `./src/${fileName}`
-      const destination = outputDir + path.sep + fileName
+    /**
+    * Step 3
+    * Copy file.html to './dist' directory
+    */
+    callback => {
+      const source = HTMLSourceFIlePath
+      const destination = HTMLDestinationFilePath
       fs.copy(source, destination, err => {
-        if (err) callback(true, `ERROR-step2 ${err}`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-3`))
+          callback(true, err)
+        }
         callback(false, `${source} exported`)
       })
     },
-    callback => { // Step 3 extract read/write data from index.html 
-      const fileName = 'index.html'
-      const file = outputDir + path.sep + fileName
+    /**
+    * Step 4
+    * Update './dist/file.html'
+    */
+    callback => {
+      // Step 4-A 
+      // read data from './dist/file.html'
+      // store data into fileNewContent variable
+      // replace href value in CSS link tag with CSSFileName 
+      // replace bundle.js, add hash if any
+      const file = HTMLDestinationFilePath
       fs.readFile(file, 'utf-8', (err, data) => {
-        if (err) callback(true, `ERROR-step3 ${err}`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-4-A`))
+          callback(true, err)
+        }
+        const regex1 = new RegExp( `href="css/${CSSSource}"`, 'g' )
+        const replace1 = `href="${addHash(CSSFileName, isHash, hash)}"`
         // working with data
-        htmlNewContent = data
-          .replace(/css\/main.css/gim, 'main.min.css') // CSS mini
-          .replace(/app\/js\/bundle.js/gim, 'bundle.js') // new path
-        callback(false, `./src/${fileName} parsed`)
+        fileNewContent = data
+          .replace(regex1, replace1)
+          .replace(/app\/js\/bundle.js/gim, addHash('bundle.js', isHash, hash))
+        callback(false, `${HTMLSourceFIlePath} parsed`)
       })
     },
-    callback => { // Step 4 write html content
-      const fileName = 'index.html'
-      const destination = outputDir + path.sep + fileName
-      fs.writeFile(destination, htmlNewContent, 'utf-8', function (err) {
-        if (err) callback(true, `ERROR-step4 ${err}`)
-        callback(false, `./dist/${fileName} updated`)
-        // reset htmlNewContent
-        htmlNewContent = null
+    callback => {
+      // Step 4-B
+      // write/replace './dist/file.html' content
+      // using the variable fileNewContent set in the
+      // previous step 4-A
+      const destination = HTMLDestinationFilePath
+      fs.writeFile(destination, fileNewContent, 'utf-8', function (err) {
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-4-B`))
+          callback(true, err)
+        }
+        callback(false, `./dist/${HTMLFileName} updated`)
+        // reset fileNewContent
+        fileNewContent = undefined
       })
     },
-    callback => { // Step 5 copy bundle.js to ouput folder
+    /**
+    * Step 5
+    * Copy './src/app/js/bundle.js' to './dist/bundle.js' directory 
+    */
+    callback => {
       const fileName = 'bundle.js'
       const source = `./src/app/js/${fileName}`
-      const destination = outputDir + path.sep + fileName
+      const destination = `${outputDir}/${addHash(fileName, isHash, hash)}`
       fs.copy(source, destination, err => {
-        if (err) callback(true, `ERROR-step5 ${err}`)
-        callback(false, `${source} exported`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-5`))
+          callback(true, err)
+        }
+        callback(false, `${source} exported ${isHash ? 'with hash' : ''}`)
       })
     },
-    callback => { // Step 6 copy main.min.css to ouput folder
-      const fileName = 'main.min.css'
-      const source = `./src/css/${fileName}`
-      const destination = outputDir + path.sep + fileName
+    /**
+    * Step 6
+    * Copy './src/css/file.css' to './dist/files.css'
+    */
+    callback => {
+      const source = CSSSourceFilePath
+      const destination = CSSDestinationFilePath
       fs.copy(source, destination, err => {
-        if (err) callback(true, `ERROR-step6 ${err}`)
-        callback(false, `${source} exported`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-6`))
+          callback(true, err)
+        }
+        callback(false, `${source} exported ${isHash ? 'with hash!!' : ''}`)
       })
     },
-    callback => { // Step 7 copy main.min.css.map to ouput folder
-      const fileName = 'main.min.css.map'
-      const source = `./src/css/${fileName}`
-      const destination = outputDir + path.sep + fileName
-      fs.copy(source, destination, err => {
-        if (err) callback(true, `ERROR-step7 ${err}`)
-        callback(false, `${source} exported`)
-      })
+    /**
+    * Step 7
+    * Copy './src/css/file.css.map' to './dist/file.css.map' if any
+    */
+    callback => {
+      if (isSourceMap) {
+        const source = CSSSourceFilePath + '.map'
+        const destination = CSSDestinationFilePath + '.map'
+        fs.copy(source, destination, err => {
+          if (err) {
+            console.log(chalk.red(`ERROR-build-task-step-7`))
+            callback(true, err)
+          }
+          callback(false, `${source} exported ${isHash ? 'with hash' : ''}`)
+        })
+      } else {
+        callback(false, 'step aborted')
+      }
     },
-    callback => { // Step 8 create ./dist/img subfolder
+    /**
+    * Step 8
+    * Update source map file if any
+    */
+    callback => { 
+      // Step 8-A 
+      // read data from './dist/hash.file.css.map'
+      // store data into fileNewContent variable
+      // replace the value of the "file" key with
+      // the CSSFileName 
+      if (isSourceMap) {
+        const file = CSSDestinationFilePath + '.map'
+        fs.readFile(file, 'utf-8', (err, data) => {
+          if (err) {
+            console.log(chalk.red(`ERROR-build-task-step-8-A`))
+            callback(true, err)
+          }
+          // working with data
+          const regex = new RegExp( CSSFileName, 'g' )
+          fileNewContent = data
+            .replace(regex, `${addHash(CSSFileName, isHash, hash)}`)
+          callback(false, `${file} parsed`)
+        })
+      } else {
+        callback(false, 'step aborted') // no source map case
+      }
+    },
+    callback => { 
+      // Step 8-B 
+      // write/replace ./dist/hash.file.css.map content
+      // using the variable fileNewContent set in the
+      // previous step 8-A
+      if (isSourceMap) {
+        const destination = CSSDestinationFilePath + '.map'
+        fs.writeFile(destination, fileNewContent, 'utf-8', function (err) {
+          if (err) {
+            console.log(chalk.red(`ERROR-build-task-step-8-B`))
+            callback(true, err)
+          }
+          callback(false, `${destination} updated`)
+          // reset fileNewContent
+          fileNewContent = undefined
+        })
+      } else {
+        callback(false, 'step aborted') // no source map case
+      }
+    },
+    /**
+    * Step 9
+    * Update CSS file source mapping URL if any
+    */
+    callback => { 
+      // Step 9-A
+      // read data from './dist/hash.file.css'
+      // store data into fileNewContent variable
+      // replace the source mapping URL with
+      // the CSSFileName 
+      if (isSourceMap) {
+        const file = CSSDestinationFilePath
+        fs.readFile(file, 'utf-8', (err, data) => {
+          if (err) {
+            console.log(chalk.red(`ERROR-build-task-step-9-A`))
+            callback(true, err)
+          }
+          // working with data
+          const regex = new RegExp( CSSFileName, 'g' )
+          fileNewContent = data
+            .replace(regex, `${addHash(CSSFileName, isHash, hash)}`)
+          callback(false, `${file} parsed`)
+        })
+      } else {
+        callback(false, 'step aborted') // no source map case
+      }
+    },
+    callback => { 
+      // Step 9-B
+      // write/replace './dist/hash.file.css 'content
+      // using the variable fileNewContent set in the
+      // previous step 10-A
+      if (isSourceMap) {
+        const destination = CSSDestinationFilePath
+        fs.writeFile(destination, fileNewContent, 'utf-8', function (err) {
+          if (err) {
+            console.log(chalk.red(`ERROR-build-task-step-8-B`))
+            callback(true, err)
+          }
+          callback(false, `${CSSDestinationFilePath} updated`)
+          // reset fileNewContent
+          fileNewContent = undefined
+        })
+      } else {
+        callback(false, 'step aborted') // no source map case
+      }
+    },
+    /**
+    * Step 10
+    * Copy/export './src/img' to './dist/img'
+    */
+    callback => { 
       const folder = 'img'
       const source = `./src/${folder}`
       const destination = outputDir + path.sep + folder
       fs.copy(source, destination, err => {
-        if (err) callback(true, `ERROR-step9 ${err}`)
+        if (err) {
+          console.log(chalk.red(`ERROR-build-task-step-10`))
+          callback(true, err)
+        }
         callback(false, `${source} exported`)
       })      
     },
-  ], (err, results) => {
+  ], (err, results) => { // Results
     if (err) {
-      console.log('build task failed!')
+      console.log(chalk.red('build task failed!'))
       console.log(`ERROR: ${err}`)
     } else {
-      results.forEach( res => {
-        console.log(res)
+      results.filter( res => {
+
+        if (res != 'step aborted') {
+          console.log(res)        
+        }
       })
       console.log(chalk.green('build task success!'))
+      // console.log(chalk.cyan(`fileNewContent? ${fileNewContent}`))
+      // console.log(chalk.cyan(`isSourceMap? ${isSourceMap}`))
     }
   })
 }
 
-buildTask()
+buildTasks()
--- bin/lib.js
+++ bin/lib.js
@@ -6,13 +6,62 @@ const fs = require('fs')
 const path = require ('path')
 
 /**
- * Return last segment in path
+ * Return last segment in path as file name
  * @param {string} string the given path as a string
  * @returns {string} the cleaned string
  */
-const dirStrName = str => `./${str.split(path.sep).reverse()[0]}`
+const dirStrName = str => `${str.split(path.sep).reverse()[0]}`
+
+/**
+ * Take a file name and return it with
+ * a hash prefix according to boolean
+ * 
+ * @param  {string} string the file name
+ * @param  {boolean} boolean the boolean
+ * @param  {string} string the hash 
+ * @returns {string} the input string prefixes with the hash
+ */
+const addHash = (str, boolean, hash) => {
+  if (boolean === true) {
+    return `${hash}.${str}`
+  } else {
+    return str
+  }
+}
+
+/**
+ * Find all files recursively in specific folder with specific extension, e.g:
+ * findFilesInDir('./project/src', '.html') ==> ['./project/src/a.html','./project/src/build/index.html']
+ * @param  {String} startPath    Path relative to this file or other file which requires this files
+ * @param  {String} filter       Extension name, e.g: '.html'
+ * @return {Array}               Result files with path string in an array
+ * @link https://stackoverflow.com/questions/25460574/find-files-by-extension-html-under-a-folder-in-nodejs/25478516#25478516
+ */
+function findFilesInDir(startPath,filter){
+
+  var results = [];
+
+  if (!fs.existsSync(startPath)){
+      // console.log("no dir ",startPath);
+      return;
+  }
+
+  var files=fs.readdirSync(startPath);
+  for(var i=0;i<files.length;i++){
+      var filename=path.join(startPath,files[i]);
+      var stat = fs.lstatSync(filename);
+      if (stat.isDirectory()){
+          results = results.concat(findFilesInDir(filename,filter)); //recurse
+      }
+      else if (filename.indexOf(filter)>=0) {
+          // console.log('-- found: ',filename);
+          results.push(filename);
+      }
+  }
+  return results;
+}
 
 /**
  * Exports
  */
-module.exports = {dirStrName}
+module.exports = {dirStrName, addHash, findFilesInDir}
